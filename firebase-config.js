d91461616a98bb09143efd5b268a7a4c7a4e2deb
Animaplays <animaplays@github.com>
2026-09-10 18:57:58 -0300

    Config Firebase para projeto separado animaplay-contador-visitas

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-/* Anima Play — Firebase config (somente leitura, sem auth) */
+/* Anima Play — Firebase config (projeto separado: animaplay-contador-visitas) */
 var firebaseConfig = {
-  apiKey: "AIzaSyDCB2t20CMsTX8N-Y_eqhNL-GIZDjn1GUo",
-  authDomain: "kingveio-contador-visitas.firebaseapp.com",
-  databaseURL: "https://kingveio-contador-visitas-default-rtdb.firebaseio.com",
-  projectId: "kingveio-contador-visitas",
-  storageBucket: "kingveio-contador-visitas.firebasestorage.app",
-  messagingSenderId: "908769132075",
-  appId: "1:908769132075:web:1f604f3139be12c726b46e",
-  measurementId: "G-0XT8MFQH5D"
+  apiKey: "AIzaSyBrmFsmd-9rFjyqtnW3qEC9iWlgZb96reY",
+  authDomain: "animaplay-contador-visitas.firebaseapp.com",
+  databaseURL: "https://animaplay-contador-visitas-default-rtdb.firebaseio.com",
+  projectId: "animaplay-contador-visitas",
+  storageBucket: "animaplay-contador-visitas.firebasestorage.app",
+  messagingSenderId: "178738859011",
+  appId: "1:178738859011:web:bc0f1ec6d6151ffe8a1236"
 };
